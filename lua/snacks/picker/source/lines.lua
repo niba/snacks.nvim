ee322226af65cf38072134052d4986f7ec070eff
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-14 11:55:45 +0100

    perf(picker.lines): no need to run in async context

--- lua/snacks/picker/source/lines.lua
+++ lua/snacks/picker/source/lines.lua
@@ -10,21 +10,18 @@ function M.lines(opts)
   buf = buf == 0 and vim.api.nvim_get_current_buf() or buf
   local extmarks = require("snacks.picker.util.highlight").get_highlights({ buf = buf })
   local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-
-  ---@async
-  ---@param cb async fun(item: snacks.picker.finder.Item)
-  return function(cb)
-    for l, line in ipairs(lines) do
-      ---@type snacks.picker.finder.Item
-      local item = {
-        buf = buf,
-        text = line,
-        pos = { l, 0 },
-        highlights = extmarks[l],
-      }
-      cb(item)
-    end
+  local items = {} ---@type snacks.picker.finder.Item[]
+  for l, line in ipairs(lines) do
+    ---@type snacks.picker.finder.Item
+    local item = {
+      buf = buf,
+      text = line,
+      pos = { l, 0 },
+      highlights = extmarks[l],
+    }
+    items[#items + 1] = item
   end
+  return items
 end
 
 return M
